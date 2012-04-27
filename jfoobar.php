69a87a0f5eb62a5a7a18497ca3e06cc2fd79a711
Niels Braczek <nbraczek@bsds.de>
2012-04-27 13:55:49 +0200

    Replaced JRequest with JInput

--- jfoobar.php
+++ jfoobar.php
@@ -8,34 +8,35 @@
 defined('_JEXEC') or die;
 
 /** 1. retrieve input */
-$singular = JRequest::getCmd('singular');
-$plural = JRequest::getCmd('plural');
+$input = JFactory::getApplication()->input;
+$singular = $input->get('singular', '', 'cmd');
+$plural = $input->get('plural', '', 'cmd');
 
 /** 2. errors */
 if ($singular == '' || $plural == '') {
     echo JText::_('COM_JFOOBAR1');
-    echo JText::_('COM_JFOOBAR2').'<br /><br />';
-    echo JText::_('COM_JFOOBAR3').'<br /><br />';
-    echo '<strong>'.JURI::base().JText::_('COM_JFOOBAR4').'</strong>'.'<br /><br />';
-    echo JText::_('COM_JFOOBAR5').'<br /><br />';
-    echo JText::_('COM_JFOOBAR6').'<br /><br />';
-    echo JText::_('COM_JFOOBAR7').'<br /><br />';
+    echo JText::_('COM_JFOOBAR2') . '<br /><br />';
+    echo JText::_('COM_JFOOBAR3') . '<br /><br />';
+    echo '<strong>' . JURI::base() . JText::_('COM_JFOOBAR4') . '</strong>' . '<br /><br />';
+    echo JText::_('COM_JFOOBAR5') . '<br /><br />';
+    echo JText::_('COM_JFOOBAR6') . '<br /><br />';
+    echo JText::_('COM_JFOOBAR7') . '<br /><br />';
     echo JText::_('COM_JFOOBAR8');
     return;
 }
 
 /** 3. copy, rename, replace literals and install */
-DEFINE('MOLAJO', 1);
-JRequest::SetVar('createtype', 'component');
-JRequest::setVar('singular', $singular);
-JRequest::setVar('plural', $plural);
-JRequest::setVar('source', 'jfoobars');
-include_once dirname(__FILE__).'/media/create.php';
+define('MOLAJO', 1);
+$input->set('createtype', 'component');
+$input->set('singular', $singular);
+$input->set('plural', $plural);
+$input->set('source', 'jfoobars');
+include_once __DIR__ . '/media/create.php';
 
 $create = new InstallerModelCreate();
 $results = $create->create();
 
 /** 4. redirect to new component */
-$controller	= JController::getInstance('jfoobar');
-$controller->setRedirect(JRoute::_('index.php?option=com_categories&extension=com_'.$plural, false));
+$controller = JController::getInstance('jfoobar');
+$controller->setRedirect(JRoute::_('index.php?option=com_categories&extension=com_' . $plural, false));
 $controller->redirect();
