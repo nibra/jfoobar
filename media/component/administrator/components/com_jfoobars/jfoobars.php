ffba4b5ff2c4e7f236dad6883bde220ef4e6948a
Niels Braczek <nbraczek@bsds.de>
2012-04-27 16:43:09 +0200

    Removed calls to deprecated methods

--- media/component/administrator/components/com_jfoobars/jfoobars.php
+++ media/component/administrator/components/com_jfoobars/jfoobars.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * @version     1.0.0
- * @package     com_jfoobars
- * @copyright   Copyright (C) 2011 Amy Stephen. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @version   1.0.0
+ * @package   com_jfoobars
+ * @copyright Copyright (C) 2011 Amy Stephen. All rights reserved.
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die;
 
-// Access check.
+// Access check
 if (!JFactory::getUser()->authorise('core.manage', 'com_jfoobars')) {
-	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
+    throw new RuntimeException(JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
-// Include dependancies
+// Include dependencies
 jimport('joomla.application.component.controller');
 
-$controller	= JController::getInstance('JfoobarsAdmin');
-$controller->execute(JRequest::getCmd('task'));
+$controller = JController::getInstance('JfoobarsAdmin');
+$controller->execute(JFactory::getApplication()->input->get('task', '', 'cmd'));
 $controller->redirect();
